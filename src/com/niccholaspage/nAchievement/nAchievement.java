683576d985c43aa955c88382a317a6c69cb77117
niccholaspage <niccholaspage@gmail.com>
2011-04-21 20:41:52 -0400

    Permissions. Done.

--- src/com/niccholaspage/nAchievement/nAchievement.java
+++ src/com/niccholaspage/nAchievement/nAchievement.java
@@ -7,9 +7,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+import org.bukkit.plugin.Plugin;
 
 public class nAchievement extends JavaPlugin {
 	private HashMap<String, Achievement> achievements = new HashMap<String, Achievement>();
+	private PermissionHandler Permissions;
     @Override
 	public void onDisable() {
 		//Print "Basic Disabled" on the log.
@@ -24,9 +28,21 @@ public class nAchievement extends JavaPlugin {
         for (int i = 0; i < Achievement.values().length; i++){
         	achievements.put(Achievement.values()[i].name().toLowerCase().replace("_", ""), Achievement.values()[i]);
         }
+        //Setup Permissions
+        setupPermissions();
         //Print that the plugin has been enabled!
         System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
 	}
+    private void setupPermissions() {
+        Plugin test = this.getServer().getPluginManager().getPlugin("Permissions");
+        if (this.Permissions == null) {
+            if (test != null) {
+                this.Permissions = ((Permissions)test).getHandler();
+            } else {
+                System.out.println("Permission system not detected, only OPs will be able to use the commands.");
+            }
+        }
+    }
     private Player getPlayerStartsWith(String startsWith){
     	if (getServer().getOnlinePlayers().length == 0) return null;
     	for (int i = 0; i < getServer().getOnlinePlayers().length; i++){
@@ -39,7 +55,10 @@ public class nAchievement extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
     	if (args.length < 1) return false;
     	if (args[0].equalsIgnoreCase("add")){
-    		//Perma
+    		if (sender instanceof Player){
+    			Player player = (Player) sender;
+    			if (!(Permissions.has(player, "nAchievement.add"))) return true;
+    		}
     		if (args.length < 3){
     			sender.sendMessage("/na add achievement");
     			return true;
